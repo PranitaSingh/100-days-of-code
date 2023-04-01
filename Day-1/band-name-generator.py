47ad3e42f6cf6afb1df69aaf90705d0fbaeeeba1
Pranita Singh <singh.pranita53@gmail.com>
2023-04-01 22:22:03 +0530

    add day1

---
+++ Day-1/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to brand name generator")
+city_name = input("whats the name of city you grew up in ?\n")
+pet_name = input("what's your pet name?\n")
+print("Your band name could be " + city_name + pet_name)
---
+++ Day-1/newline.py
@@ -0,0 +1,12 @@
+#print ("Hello world!\nHello World!\nHelloworld")
+# print ("hello "+"pranita")
+# print ("Hello"+" "+"Pranita")
+# print ("Hello "+ input("what is your name ?")+"!")
+
+# print("your full name is "+input("enter your fisrt name"+"enter your last name"))
+s1 = input("enter your fisrt name:")
+s2 = input("enter your last name:")
+# print("your full name is "+(s1)+" "+(s2))
+print(s1+s2)
+
+
